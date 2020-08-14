bc929fc7bd3f7394ec399757e720b78a844b879c
vinaykudari <svvk.20140513@btech.nitdgp.ac.in>
2020-08-15 02:46:42 +0530

    Improve face detection performance

--- mask_detection/api/predictor.py
+++ mask_detection/api/predictor.py
@@ -9,8 +9,8 @@ from torchvision.transforms import Compose, ToPILImage, Resize, ToTensor
 from django.conf import settings
 
 RES = 224
-FACE_DETECTION_CONFIDENCE = 0
-FACE_DETECTION_THRESHOLD = 0
+FACE_DETECTION_CONFIDENCE = 0.5
+FACE_DETECTION_THRESHOLD = 0.4
 MASK_DETECTION_THRESHOLD = 0.96
 
 TRANSFORMATIONS = Compose([
@@ -28,13 +28,14 @@ def get_faces_from(image):
 	img_RGB = img.convert('RGB')
 	img_mat = asarray(img_RGB)
 	face_details = []
-	face_coordinates, confidence, indexes = np.array([]), np.array([]), np.array([])
-	height, width = img_mat.shape[:2]
+	face_coordinates, confidence, indexes = [], [], np.array([])
+	img_height, img_width = img_mat.shape[:2]
 	
 	blob = cv2.dnn.blobFromImage(
 		img_mat,
 		1 / 255.,
 		(416, 416),
+		[0, 0, 0],
 		1,
 		crop=False
 	)
@@ -48,13 +49,20 @@ def get_faces_from(image):
 	)
 	
 	for out in predictions:
-		out = out[out[:, -1] > FACE_DETECTION_THRESHOLD]
-		if out.any():
-			face_coordinates = out[:, :4] * np.array([width, height, width, height]).T
-			face_coordinates = face_coordinates.astype(int)
-			face_coordinates[:, 0] = face_coordinates[:, 0] - face_coordinates[:, 2] / 2
-			face_coordinates[:, 1] = face_coordinates[:, 1] - face_coordinates[:, 3] / 2
-			confidence = out[:, 5]
+		for detection in out:
+			scores = detection[5:]
+			conf = scores[np.argmax(scores)]
+			if conf > FACE_DETECTION_THRESHOLD:
+				face_width = int(detection[2] * img_width)
+				face_height = int(detection[3] * img_height)
+				x1 = int(int(detection[0] * img_width) - face_width / 2)
+				y1 = int(int(detection[1] * img_height) - face_height / 2)
+				
+				confidence.append(float(conf))
+				face_coordinates.append([x1, y1, face_width, face_height])
+
+	confidence = np.array(confidence)
+	face_coordinates = np.array(face_coordinates)
 	
 	if face_coordinates.any() and confidence.any():
 		face_coordinates = face_coordinates.tolist()
